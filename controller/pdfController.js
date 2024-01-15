01924e0fd5e15f79b7c74f6af5abd4c1ac374afb
Mariana <marianafelixdelima7@gmail.com>
2024-01-15 18:49:56 -0300

    add controller/

---
+++ controller/pdfController.js
@@ -0,0 +1,10 @@
+//Configuração para processamento PDF
+
+const uploadPDF = (req, res)=> {
+    //Upload
+};
+const processPDF = (req, res)=> {
+    //Processamento
+}
+
+module.exports = {uploadPDF, processPDF}
