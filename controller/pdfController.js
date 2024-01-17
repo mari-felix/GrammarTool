1114df7ced567fa847076530f8485ee036f6aeb1
Mariana <marianafelixdelima7@gmail.com>
2024-01-17 12:36:31 -0300

    updaing controller/

--- controller/pdfController.js
+++ controller/pdfController.js
@@ -1,10 +1,39 @@
 //Configuração para processamento PDF
+const PDF = require('../models/pdfModel');
+const UploadService = require('../services/uploadService');
+const ReturnService = require('../services/returnService');
 
-const uploadPDF = (req, res)=> {
-    //Upload
-};
-const processPDF = (req, res)=> {
-    //Processamento
+const uploadService = new UploadService(PDF);
+const returnService = new ReturnService(PDF);
+
+class PDFController {
+    async handleUpload(req, res){
+        try {
+            const resultado = await uploadService.processUpload(req)
+            res.send(resultado)
+        } catch(err) {
+            console.error(err);
+            res.status(500).send('Erro no upload');
+        }
+    }
+
+    async handleDownload(req, res) {
+        try {
+            const pdfFilename = req.params.filename;
+    
+            const pdf = await returnService.getPDFbyFilename(pdfFilename);
+    
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', `attachment; filename=${pdf.filename}`)
+    
+            res.send(pdf.content)
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Erro ao obter PDF');
+
+        }
+    }
 }
 
-module.exports = {uploadPDF, processPDF}
+
+module.exports = new PDFController();
